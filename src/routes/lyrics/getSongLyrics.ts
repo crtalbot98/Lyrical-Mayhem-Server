ba86bbf7849ca3ea091a84c8d27971090c14096e
Chandler Talbot <crtalbot98@gmail.com>
2022-06-06 16:49:59 -0400

    added genius lyrics if textyl lyrics are missing

--- src/routes/lyrics/getSongLyrics.ts
+++ src/routes/lyrics/getSongLyrics.ts
@@ -9,24 +9,39 @@ const lyricClient = new Genius.Client(process.env.GENIUS_LYRICS_ID);
 export default router.get('/getSongLyrics', async (req: Request, res: Response): Promise<void> => {
   const songName = req.query.songName.toString();
   const artistName = req.query.artistName.toString();
+  let lyrics;
 
   try {
-    const songWithTimestampedLyrics = await axios.get(`https://api.textyl.co/api/lyrics?q=${artistName} ${songName}`);
-    const songsList = await lyricClient.songs.search(songName);
+    const lyricsWithTimestamps = await axios.get(`https://api.textyl.co/api/lyrics?q=${artistName} ${songName}`);
+    lyrics = lyricsWithTimestamps.data
+  }
+  catch(err) {
+    console.log('ERROR :: getSongLyrics :: textyl api res', err);
+    lyrics = err.response.data
+  }
 
+  try {
+    const songsList = await lyricClient.songs.search(songName);
     const songFromArtist = songsList.filter((elm) => {
       return elm.artist.name.includes(artistName)
     });
 
-    if(songFromArtist.length < 1 && songWithTimestampedLyrics.data === 'No lyrics available') {
-      res.send('No lyrics available')
+    if(songFromArtist.length >= 1 && lyrics === 'No lyrics available') {
+      const fullLyricsString = await songFromArtist[0].lyrics();
+      const lyricsArray = fullLyricsString.split('\n');
+      
+      lyrics = lyricsArray.filter((elm) => {
+        return !elm.includes('[')
+      })
+    }
+    else {
+      throw new Error('No lyrics available')
     }
 
-    const lyrics = songWithTimestampedLyrics.data !== 'No lyrics available' ? songWithTimestampedLyrics.data : await songFromArtist[0].lyrics()
-    res.send(lyrics);
+    res.send(lyrics)
   }
   catch(err) {
     console.log('ERROR :: getSongLyrics', err);
-    res.send('ERROR :: Something went wrong.');
+    res.send({ 'Error': err });
   }
 });
